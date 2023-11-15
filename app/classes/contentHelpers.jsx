006a248da2853b6e632536bae92da30741c42d7b
foglabs <foglaboratories@gmail.com>
2023-11-15 15:40:51 -0500

    'wip'

--- app/classes/contentHelpers.jsx
+++ app/classes/contentHelpers.jsx
@@ -4,7 +4,6 @@ export function renderBlocks(blocks){
   // jsx likes to be in an array to be concatted when rendered
   let output = []
   blocks.forEach( (block) => {
-    console.log( 'render this one!', block )
     output.push( renderBlock(block) )
   })
 
@@ -14,24 +13,26 @@ export function renderBlocks(blocks){
 export function renderBlock(block){
   if(block.type == "text"){
     return textContent(block)
-  } else if(block.type == "interviews"){
-    return interviewsContent(block)
-  } else if(block.type == "archivalFootage"){
-    return archivalFootageContent(block)
-  } else if(block.type == "photographs"){
-    return photographsContent(block)
-  } else if(block.type == "originalFootage"){
-    return originalFootageContent(block)
-  } else if(block.type == "relatedContent"){
-    return relatedContentContent(block)
-  } else if(block.type == "credits"){
-    return creditsContent(block)
+  // } else if(block.type == "interviews"){
+  //   return interviewsContent(block)
+  // } else if(block.type == "archivalFootage"){
+  //   return archivalFootageContent(block)
+  // } else if(block.type == "photographs"){
+  //   return photographsContent(block)
+  // } else if(block.type == "originalFootage"){
+  //   return originalFootageContent(block)
+  // } else if(block.type == "relatedContent"){
+  //   return relatedContentContent(block)
+  // } else if(block.type == "credits"){
+  //   return creditsContent(block)
   } else if(block.type == "heading"){
     return headingContent(block)
   } else if(block.type == "image"){
     return imageContent(block)
   } else {
-    return (<div key={ block.id }>I DONT FEEL LIKE IT</div>)
+    // lol
+    // return (<div key={ block.id }>I DONT FEEL LIKE IT</div>)
+    return contentBlock(block)
   }
 }
 
@@ -49,6 +50,10 @@ export function headingContent(block){
       { block.value }
     </div>  
   )
+}
+
+export function contentBlock(block){
+  <div className="content-block" dangerouslySetInnerHTML={{ __html: decode(block.value) }} />
 }
 
 export function creditsContent(block){
