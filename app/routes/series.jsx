cd3eac44cb1a2d0a92ef7a91e98ee3814c600776
foglabs <foglaboratories@gmail.com>
2024-01-05 12:18:00 -0500

    'fix series titles, convert series to component to use AAPB_HOST, few fontsize fixes'

--- app/routes/series.jsx
+++ app/routes/series.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData } from "@remix-run/react"
-import React, { useState } from "react"
+import React, { Component } from "react"
 import { renderAuthorBubble, renderPageLink, renderPageLinks } from "~/classes/pageHelpers"
 import { seriesData } from "~/data/seriesData"
 
@@ -14,51 +14,65 @@ function renderSeriesLink(series){
   )
 }
 
-export default function Series() {
-  const [seriesSearch, setSeriesSearch] = useState("")
-  
-  let alphabetLinks = Object.keys(seriesData).map( (letter) => { return (<a className="series-alphabet-link" href={ "#series-"+letter.toLowerCase() } >{ letter }</a>) } )
+export default class Series extends Component {
+  constructor(props){  
+    super(props)
 
-  let seriesAlphaGroups = Object.keys(seriesData).map( (letter) => {
-    let seriesGroup = seriesData[letter]
-    if(seriesSearch.length > 0){
-      seriesGroup = seriesGroup.filter( (title) => title.toLowerCase().includes(seriesSearch) )
+    this.state = {
+      seriesSearch: ""
     }
-    seriesGroup = seriesGroup.map( (title) => { return <a className="series-link" href="https://americanarchive.org" >{ title }</a> })
+  }
 
-    return(
-      <div className="series-group">
-        <div id={ "series-"+letter.toLowerCase() } className="series-group-letter">{ letter }</div>
-        { seriesGroup }
+  componentDidMount(){
+    // this is a bit annoying, but because we're injecting AAPB_HOST into window, use this hook to grab AAPB_HOST once its available
+    this.setState({aapb_host: window.ENV.AAPB_HOST})
+  }
+
+  render(){
+
+    let alphabetLinks = Object.keys(seriesData).map( (letter) => { return (<a className="series-alphabet-link" href={ "#series-"+letter.toLowerCase() } >{ letter }</a>) } )
+
+    let seriesAlphaGroups = Object.keys(seriesData).map( (letter) => {
+      let seriesGroup = seriesData[letter]
+      if(this.state.seriesSearch.length > 0){
+        seriesGroup = seriesGroup.filter( (title) => title.toLowerCase().includes(seriesSearch) )
+      }
+      
+      seriesGroup = seriesGroup.map( (title) => { return <a className="series-link" href={ `${ this.state.aapb_host }/catalog?f[series_titles][]=${ title }&f[access_types][]=all` } >{ title }</a> })
+
+      return(
+        <div className="series-group">
+          <div id={ "series-"+letter.toLowerCase() } className="series-group-letter">{ letter }</div>
+          { seriesGroup }
+        </div>
+      )
+    })
+
+    // seriesAlphaGroups = seriesAlphaGroups.filter( (sG) => sG.length > 0  )
+    return (
+      <div className="page-container">
+        <div className="page-sidebar">
+          <div className="page-sidebar-title spaced">Search GBH Series</div>
+          <div className="series-search-container">
+            <input className="series-search" onKeyUp={ (e) => { this.setState({aapb_host: e.target.value}) } } type="text" name="series-search" placeholder="Series Name" />
+            <div className="series-search-button" />
+          </div>
+
+          <div className="page-sidebar-title spaced">Jump To</div>
+          <div className="series-alphabet">
+            { alphabetLinks }
+          </div>
+        </div>
+
+        <div className="page-body-container">
+          <h1 className="series-bigtitle">GBH Series</h1>
+          <div className="series-summary">
+            Browse by title and explore records on AAPB
+          </div>
+          { seriesAlphaGroups  }          
+        </div>
       </div>
     )
-  })
+  }
 
-  // seriesAlphaGroups = seriesAlphaGroups.filter( (sG) => sG.length > 0  )
-
-  return (
-    <div className="page-container">
-      <div className="page-sidebar">
-        <div className="page-sidebar-title spaced">Search GBH Series</div>
-        <div className="series-search-container">
-          <input className="series-search" onKeyUp={ (e) => { setSeriesSearch(e.target.value) } } type="text" name="series-search" placeholder="Series Name" />
-          <div className="series-search-button" />
-        </div>
-
-        <div className="page-sidebar-title spaced">Jump To</div>
-        <div className="series-alphabet">
-          { alphabetLinks }
-        </div>
-      </div>
-
-      <div className="page-body-container">
-        <h1 className="series-bigtitle">GBH Series</h1>
-        <div className="series-summary">
-          Browse by title and explore records on AAPB
-        </div>
-        { seriesAlphaGroups  }          
-      </div>
-      
-    </div>
-  )
 }
