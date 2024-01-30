79b2012884192defaaeaed0ea12becdfb0bb5cb1
Harpo Harbert <ryan_harbert@wgbh.org>
2024-01-30 15:04:39 -0800

    Working AAPBResults component with debounce and updating state!!!

--- search/utils.js
+++ search/utils.js
@@ -1,5 +1,7 @@
-import { useInstantSearch } from 'react-instantsearch';
+import { useInstantSearch, useSearchBox } from 'react-instantsearch';
 import './spinner.css';
+import { useState, useEffect, useCallback } from 'react';
+import { debounce } from 'lodash';
 
 export function Error() {
     const { error } = useInstantSearch({ catchError: true });
@@ -11,24 +13,39 @@ export function Error() {
 
 export function Spinner() {
     return <>
-        <div class="spinner">
-            <div class="rect1"></div>
-            <div class="rect2"></div>
-            <div class="rect3"></div>
-            <div class="rect4"></div>
-            <div class="rect5"></div>
+        <div className="spinner">
+            <div className="rect1"></div>
+            <div className="rect2"></div>
+            <div className="rect3"></div>
+            <div className="rect4"></div>
+            <div className="rect5"></div>
         </div>
     </>
 }
 
 export function AAPBResults() {
+    const [results, setResults] = useState(null);
+    const { query } = useSearchBox();
+
+    const fetchResults = useCallback(debounce((currentQuery) => {
+        fetch(`https://americanarchive.org/api.json?q=${currentQuery}&rows=0`)
+            .then(response => response.json())
+            .then(data => setResults(data.response.numFound))
+            .catch(error => console.error(error));
+    }, 500), []);
+
+    useEffect(() => {
+        setResults(null); // Set results to null when the query changes to show the spinner
+        fetchResults(query);
+    }, [fetchResults, query]);
+
     return (
         <>
             <a href="#">
                 Search AmericanArchive.org
                 &#x21E8;
                 <span className="ais-RefinementList-count">
-                    <Spinner />
+                    {results !== null ? results : <Spinner />}
                 </span>
             </a>
         </>
