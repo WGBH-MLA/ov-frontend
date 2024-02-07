309c7b5c5d0ccfbf520541b11dcc01f9aa507f21
Harpo Harbert <ryan_harbert@wgbh.org>
2024-02-07 13:38:51 -0800

    Fixes CSS stylesheet inclusion

--- app/classes/openCarousel.jsx
+++ app/classes/openCarousel.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react"
 import { Carousel } from "react-responsive-carousel"
+import "../../node_modules/react-responsive-carousel/lib/styles/carousel.min.css"
 import '../styles/carousel.css'
 
 export class OpenCarousel extends Component {
