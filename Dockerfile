7bf68da86e31a9d24c4d33b55d5c30541488effe
Harpo Harbert <ryan_harbert@wgbh.org>
2024-05-22 15:47:53 -0700

    Updates package versions

--- Dockerfile
+++ Dockerfile
@@ -11,10 +11,12 @@ CMD npm run dev
 
 # Production
 FROM node:alpine AS production
+ENV PATH="${PATH}:/app/node_modules/.bin"
+ENV NODE_ENV=production
 
 COPY ./ .
 RUN npm i
 RUN npm run build
 
-ENV NODE_ENV=production
+
 CMD npm run start
--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,36 @@
 /**
  * @type {import('@remix-run/dev/config').AppConfig}
-*/
+ */
+import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react'
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
-
 
 export default defineConfig({
-    plugins: [reactRefresh(), remix({
-        appDirectory: "app",
-        buildDirectory: "public/build",
-        publicPath: "/build/",
-        serverBuildFile: "./build/index.js",
-        ignoredRouteFiles: [".*"],
-        serverModuleFormat: "esm",
-    })],
-    server: {
-        port: 3000,
-        host: '0.0.0.0',
-        fs: {
-            strict: true,
-        },
-        hmr:
-            { overlay: false }
+  plugins: [
+    reactRefresh(),
+    remix({
+      appDirectory: 'app',
+      buildDirectory: 'public/build',
+      publicPath: '/build/',
+      serverBuildFile: './build/index.js',
+      ignoredRouteFiles: ['.*'],
+      serverModuleFormat: 'esm',
+    }),
+  ],
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    fs: {
+      strict: true,
     },
-    css: {
-        modules: {
-            localsConvention: 'camelCaseOnly',
-        },
+    hmr: { overlay: false },
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
     },
-    esbuild: {
-        jsxInject: `import React from 'react'`
-    }
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
 })
