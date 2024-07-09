9c35217dd1188b7b4b5078de2126be1b6c7d3eb1
Harpo Harbert <ryan_harbert@wgbh.org>
2024-07-09 13:01:20 -0700

    Fixes series carousel highlight

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -1,22 +1,27 @@
 import { useInstantSearch } from 'react-instantsearch-core'
+import { Highlight, Hits } from 'react-instantsearch'
 
 export const Carousel = ({ aapb_host }) => {
   const { results } = useInstantSearch()
-  console.log('carousel', results)
-  return (
-    <div className="series-carousel">
-      {results.hits.map((hit, index) => (
-        <div key={index} className="series-carousel-item">
-          <div className="tag">GBH Series</div>
+  const aapb_link = (title) => `${aapb_host}/catalog?f[series_titles][]=${title}&q=+(contributing_organizations: WGBH(MA) OR producing_organizations: WGBH Educational Foundation)&f[access_types][]=all`
 
-          <a
-            href={`${aapb_host}/catalog?f[series_titles][]=${hit.title}&q=+(contributing_organizations: WGBH(MA) OR producing_organizations: WGBH Educational Foundation)&f[access_types][]=all`}
-            target="_blank"
-          >
-            {hit.title}
-          </a>
-        </div>
-      ))}
-    </div>
+  const SeriesHit = ({ hit }) => {
+    return (
+      <a href={aapb_link(hit.title)} target="_blank">
+        <div className="tag">GBH Series</div>
+        <Highlight attribute="title" hit={hit} />
+      </a>
+    )
+  }
+  
+
+  return (
+    <Hits
+      hitComponent={SeriesHit}
+      classNames={{
+        list: 'series-carousel',
+        item: 'series-carousel-item',
+      }}
+    />
   )
 }
