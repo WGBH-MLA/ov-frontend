83cba57c9a6f5c5787d52304720a673c88b4f571
fog <foglaboratories@gmail.com>
2024-07-17 14:51:50 -0400

    'fix pagelink colors, author name not link, big screens css'

--- app/classes/pageHelpers.jsx
+++ app/classes/pageHelpers.jsx
@@ -43,8 +43,8 @@ export function renderPageLink(pageType, page, key){
         <div className="pagelink-image" style={{ backgroundImage: page.cover_image ? "url(" + page.cover_image.full_url + ")" : null }}></div>
         <h4 className="pagelink-title">{ page.title }</h4>
         { authorBubble }
-        { authorLink }
       </a>
+      { authorLink }
     </div>
   )
 }
