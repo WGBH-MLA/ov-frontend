a492f0d1e8cfcbdb2bad1048df56d63a404499f9
Harpo Harbert <ryan_harbert@wgbh.org>
2024-07-23 13:22:22 -0700

    Possible structure for old OV /exhibits/

--- app/exhibit.jsx
+++ app/exhibit.jsx
@@ -1,3 +1,4 @@
+import { redirect } from '@remix-run/node'
 import { fetch2 } from './fetch'
 
 export async function getExhibits() {
@@ -5,5 +6,28 @@ export async function getExhibits() {
 }
 
 export async function getExhibit(id) {
-  return await fetch2(process.env.OV_API_URL + `/api/v2/exhibits/${id}/`)
+  return await fetch2(process.env.OV_API_URL + `/api/v2/exhibits/${id}/`).catch(
+    async error => {
+      if (error.status === 404) {
+        console.log('Exhibit not found. Checking if this is an old Open Vault URL...')
+        // If there's no matching exhibit from the API,
+        // check if this might be an old Open Vault URL.
+
+        // This service is not yet implemented,
+        // but using it would look something like this:
+        await fetch(`${process.env.ORGAN_URL}/v1/ov/exhibits/${id}`)
+          .then(res => {
+            if (res?.status === 200) {
+              console.log('Found old exhibit', res)
+              let exhibit = res.json()
+              return redirect(`/exhibits/${exhibit.id}`)
+            }
+          })
+          .catch(err => {
+            console.error('error resolving exhibit', err)
+          })
+      }
+      throw error
+    }
+  )
 }
