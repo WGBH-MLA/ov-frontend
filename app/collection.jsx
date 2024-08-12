110abd0350ba0bd0fcebc280d5a1b491da44aa8b
fog <foglaboratories@gmail.com>
2024-08-12 17:42:41 -0400

    'remove fetch2'

--- app/collection.jsx
+++ app/collection.jsx
@@ -1,9 +1,7 @@
-import { fetch2 } from './fetch'
-
 export async function getCollections() {
-  return await fetch2(process.env.OV_API_URL + '/api/v2/collections/')
+  return await fetch(process.env.OV_API_URL + '/api/v2/collections/')
 }
 
 export async function getCollection(id) {
-  return await fetch2(process.env.OV_API_URL + `/api/v2/collections/${id}/`)
+  return await fetch(process.env.OV_API_URL + `/api/v2/collections/${id}/`)
 }
