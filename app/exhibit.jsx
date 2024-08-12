498fec1cd89df1405908f3ef53d28aac0dc45dd5
fog <foglaboratories@gmail.com>
2024-08-12 17:44:10 -0400

    'remove fetch2 code because error boundary handles error bubbling'

--- app/exhibit.jsx
+++ app/exhibit.jsx
@@ -1,9 +1,7 @@
-import { fetch2 } from './fetch'
-
 export async function getExhibits() {
-  return await fetch2(process.env.OV_API_URL + '/api/v2/exhibits/')
+  return await fetch(process.env.OV_API_URL + '/api/v2/exhibits/')
 }
 
 export async function getExhibit(id) {
-  return await fetch2(process.env.OV_API_URL + `/api/v2/exhibits/${id}/`)
+  return await fetch(process.env.OV_API_URL + `/api/v2/exhibits/${id}/`)
 }
