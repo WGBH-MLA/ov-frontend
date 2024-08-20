d9eec3444e9810b82bafbb7616325b8dd6c32acb
Harpo Harbert <ryan_harbert@wgbh.org>
2024-08-20 12:36:30 -0700

    Updates browser message on FAQ, adds hyperlinks.

--- app/routes/faq.jsx
+++ app/routes/faq.jsx
@@ -13,27 +13,27 @@ export default function FAQ() {
             <h2>Frequently Asked Questions</h2>
             <div className="med-heading">I can't get one of the video or audio records to play. What should I do?</div>
             <div className="med-textline">
-              Please make sure your web browser is as new as or newer than Internet Explorer 7.0, Mozilla Firefox 3.0, or Safari 3.0. If the record still will not play, please contact us.
+              Please make sure your web browser is updated to the latest version. If the record still will not play, please <a href="/contact-us">contact us</a>.
             </div>
 
             <div className="med-heading">I have found a factual error in one of the records. Who should I contact?</div>
             <div className="med-textline">
-              Contact us here.
+              <a href="/contact-us">Contact us here.</a>
             </div>
 
             <div className="med-heading">Can I buy copies of the programs on Open Vault?</div>
             <div className="med-textline">
-              Most programs are no longer available for distribution. Please refer to www.shoppbs.org for the latest catalog of programs available for distribution.
+              Most programs are no longer available for distribution. Please refer to <a href="https://shop.pbs.org/">shop.pbs.org</a> for the latest catalog of programs available for distribution.
             </div>
             
             <div className="med-heading">Can I buy copies of source materials (interviews, news footage, b-roll, etc)?</div>
             <div className="med-textline">
-              For professional licensing requests, please contact GBH Stock Sales at 617-300-3939 or stock_sales@wgbh.org.
+              For professional licensing requests, please contact GBH Stock Sales at 617-300-3939 or <a href="mailto:stock_sales@wgbh.org?subject=Open%20Vault%20licensing">stock_sales@wgbh.org</a>.
             </div>
 
             <div className="med-heading">How can I arrange to visit the GBH Archives?</div>
             <div className="med-textline">
-              Individuals wishing to visit the Archives should be engaged in educational research. Please see Visit Us for further information.
+              Individuals wishing to visit the Archives should be engaged in educational research. Please see <a href="/visit-us">Visit Us</a> for further information.
             </div>
 
             <div className="med-heading">Can you provide me with contact information for any of the people in the clips?</div>
