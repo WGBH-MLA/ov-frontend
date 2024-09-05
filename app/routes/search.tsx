6d3fda793d17efe5659a59f53089f95171b78ba0
Harpo Harbert <ryan_harbert@wgbh.org>
2024-09-05 12:58:04 -0700

    Removes InstantSearchSSRProvider to fix expired elasticsearch cert crashing frontend server

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,24 +1,17 @@
-import { renderToString } from 'react-dom/server'
-import {
-  RefinementList,
-  getServerState,
-  InstantSearchServerState,
-} from 'react-instantsearch'
+import { RefinementList, InstantSearchServerState } from 'react-instantsearch'
 import type { LoaderFunction, MetaFunction } from '@remix-run/node'
 import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { useLoaderData, useRouteError } from '@remix-run/react'
 import { Panel } from '../components/Panel'
 import { Search } from '../classes/search-ui'
 import 'instantsearch.css/themes/algolia-min.css'
 import '../styles/search.css'
 
 export const meta: MetaFunction = ({ location }) => {
-  const query = new URLSearchParams(
-    location.search
-  ).get("q")
+  const query = new URLSearchParams(location.search).get('q')
   return [
     {
-      title: `${query ? query + ' | ' : '' }Search GBH Open Vault`,
+      title: `${query ? query + ' | ' : ''}Search GBH Open Vault`,
     },
     {
       name: 'description',
@@ -30,16 +23,9 @@ export const meta: MetaFunction = ({ location }) => {
 
 export const loader: LoaderFunction = async ({ request }) => {
   const serverUrl = request.url
-  const aapb_host = process.env.AAPB_HOST || 'https://demo.aapb.wgbh-mla.org'
-  const serverState = await getServerState(
-    <Search serverUrl={serverUrl} aapb_host={aapb_host} />,
-    {
-      renderToString,
-    }
-  )
+  const aapb_host = process.env.AAPB_HOST
 
   return json({
-    serverState,
     serverUrl,
     aapb_host,
   })
@@ -54,18 +40,28 @@ function FallbackComponent({ attribute }: { attribute: string }) {
 }
 
 export type SearchProps = {
-  serverState?: InstantSearchServerState
   serverUrl?: URL
   aapb_host?: URL
 }
 
 export default function SearchPage() {
-  const { serverState, serverUrl, aapb_host }: SearchProps = useLoaderData()
+  const { serverUrl, aapb_host }: SearchProps = useLoaderData()
   return (
     <Search
-      serverState={serverState}
       serverUrl={serverUrl}
       aapb_host={aapb_host}
     />
+  )
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError()
+  console.log('search error', error)
+  return (
+    <div>
+      <h1>Search Error</h1>
+      <h4>We're sorry! Search appears to be broken!</h4>
+      <pre>{error.message}</pre>
+    </div>
   )
 }
