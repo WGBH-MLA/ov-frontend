f1500d42a18d4de27fdbc65ad6e9a930576f1bec
Harpo Harbert <ryan_harbert@wgbh.org>
2024-09-11 08:14:39 -0700

    Updates Home meta

--- app/classes/meta.tsx
+++ app/classes/meta.tsx
@@ -16,16 +16,16 @@ export const extractMeta = (server_url: URL, article: Article) => [
   { name: 'twitter:card', content: 'summary_large_image' },
   { property: 'twitter:domain', content: 'openvault.wgbh.org' },
   { property: 'twitter:url', content: server_url },
-  { property: 'twitter:title', content: article.title },
+  { name: 'twitter:title', content: article.title },
   {
-    property: 'twitter:description',
+    name: 'twitter:description',
     content: article.meta.search_description || 'GBH Open Vault Exhibit',
   },
-  { property: 'twitter:image', content: article.cover_image.url },
+  { name: 'twitter:image', content: article.cover_image.url },
 ]
 
 export const HomeMeta = [
-  // { property: 'og:url', content: 'https://openvault.wgbh.org' },
+  { property: 'og:url', content: 'https://openvault.wgbh.org' },
   { property: 'og:type', content: 'website' },
   { property: 'og:title', content: 'GBH Open Vault' },
   {
@@ -35,14 +35,14 @@ export const HomeMeta = [
   { property: 'og:image', content: 'https://ov.wgbh-mla.org/gbh-mural.jpg' },
   { name: 'twitter:card', content: 'summary_large_image' },
   { property: 'twitter:domain', content: 'openvault.wgbh.org' },
-  // { property: 'twitter:url', content: 'https://openvault.wgbh.org' },
-  { property: 'twitter:title', content: 'GBH Open Vault Exhibit' },
+  { property: 'twitter:url', content: 'https://openvault.wgbh.org' },
+  { name: 'twitter:title', content: 'GBH Open Vault Exhibit' },
   {
-    property: 'twitter:description',
+    name: 'twitter:description',
     content: 'Explore Scholar Exhibits and Collections from GBH Open Vault',
   },
   {
-    property: 'twitter:image',
+    name: 'twitter:image',
     content: 'https://ov.wgbh-mla.org/gbh-mural.jpg',
   },
 ]
