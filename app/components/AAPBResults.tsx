93d458a31d6e3a48eeb67e56247ca1d3f8d09c2e
Harpo Harbert <ryan_harbert@wgbh.org>
2024-12-11 15:51:43 -0800

    Basic AAPBHit component!!

--- app/components/AAPBResults.tsx
+++ app/components/AAPBResults.tsx
@@ -7,42 +7,79 @@ import { Spinner } from './Spinner'
 const gbh_query =
   '+AND+(contributing_organizations:%20WGBH(MA)%20OR%20producing_organizations:%20WGBH%20Educational%20Foundation)&f[access_types][]=online'
 
+export type AAPBHit = {
+  id: string
+  title: string
+  xml: string
+}
+
+export type AAPBHitProps = {
+  hit: AAPBHit
+  aapb_host: string
+}
+
 export const AAPBResults = ({ aapb_host }) => {
   const { indexUiState } = useInstantSearch()
 
   const [result_count, setResults] = useState(null)
+  const [hits, setHits] = useState([])
 
   const fetchResults = useCallback(
     debounce((currentQuery) => {
       console.log('fetching AAPB results for', currentQuery)
       fetch(
-        `${aapb_host}/api.json?q=${encodeURIComponent(currentQuery)}&rows=0`
+        `${aapb_host}/api.json?q=${encodeURIComponent(currentQuery)}&rows=10`
       )
         .then((response) => response.json())
-        .then((data) => setResults(data.response.numFound))
+        .then((data) => {
+          setResults(data.response.numFound)
+          setHits(data.response.docs)
+        })
         .catch((error) => console.error(error))
     }, 200),
     []
   )
 
   useEffect(() => {
-    if (indexUiState.query) {
-      setResults(null) // Set results to null when the query changes to show the spinner
-      fetchResults(indexUiState.query)
-    }
+    setResults(null) // Set results to null when the query changes to show the spinner
+    fetchResults(indexUiState.query || '')
   }, [fetchResults, indexUiState])
 
   return (
-    indexUiState.query && (
+    <>
+      Found{' '}
+      <span className='ais-RefinementList-count'>
+        {result_count === null ? <Spinner /> : result_count}
+      </span>{' '}
+      matching records on AmericanArchive.org for "{indexUiState.query}"
+      <div className='ais-Hits'>
+        <div className='ais-Hits-list'>
+          {hits.map((hit: AAPBHit) => (
+            <AAPBHit key={hit.id} hit={hit} aapb_host={aapb_host} />
+          ))}
+        </div>
+      </div>
       <a
         href={`${aapb_host}/catalog?q=${indexUiState.query}${gbh_query}`}
         target='_blank'>
-        Found{' '}
+        See{' '}
         <span className='ais-RefinementList-count'>
           {result_count === null ? <Spinner /> : result_count}
         </span>{' '}
-        matching records on AmericanArchive.org for "{indexUiState.query}"
+        more matching records on AmericanArchive.org for "{indexUiState.query}"
       </a>
-    )
+    </>
+  )
+}
+
+export const AAPBHit = ({ hit, aapb_host }: AAPBHitProps) => {
+  return (
+    <div className='ais-Hits-item aapb-hit'>
+      <a href={`${aapb_host}/catalog/${hit.id}`}>
+        <div className='tag'>AAPB</div>
+        <h3>{hit.title}</h3>
+        <pre>{hit.xml}</pre>
+      </a>
+    </div>
   )
 }
