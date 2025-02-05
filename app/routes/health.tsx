aecd64c5628a5b16c9ff62613ec4a345bcb75451
Harpo Harbert <ryan_harbert@wgbh.org>
2025-02-05 13:05:18 -0800

    Create `/health` status route Fixes #192

---
+++ app/routes/health.tsx
@@ -0,0 +1,9 @@
+/*Health Check
+
+This is a simple health check route that returns a 200 status code and a JSON response with a status of "ok". This is monitored by the cluster to ensure the app is running, and is available for use by monitoring tools.
+*/
+
+import type { LoaderFunction } from '@remix-run/node'
+
+export const loader: LoaderFunction = async () =>
+  Response.json({ status: 'ok' })
